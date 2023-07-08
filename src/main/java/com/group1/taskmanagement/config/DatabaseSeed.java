95a7652b6e018f0e4186d09dec5229271e8ae16f
ConnorDBurge <connor.d.burge@gmail.com>
2023-07-08 14:10:51 -0400

    changed SQL seed

--- src/main/java/com/group1/taskmanagement/config/DatabaseSeed.java
+++ src/main/java/com/group1/taskmanagement/config/DatabaseSeed.java
@@ -63,13 +63,34 @@ public class DatabaseSeed implements CommandLineRunner {
         // Insert ADMIN and USER role
         jdbcTemplate.execute("INSERT INTO roles (name) SELECT 'ADMIN' WHERE NOT EXISTS (SELECT 1 FROM roles WHERE name = 'ADMIN')");
         jdbcTemplate.execute("INSERT INTO roles (name) SELECT 'USER' WHERE NOT EXISTS (SELECT 1 FROM roles WHERE name = 'USER')");
+
         // Sign two users up
         signUpUsers(jdbcTemplate, webClient);
+
         // Assign ADMIN to admin user
-        jdbcTemplate.execute("INSERT INTO user_roles (user_id, role_id) SELECT app_users.id, roles.id FROM app_users JOIN roles ON roles.name = 'ADMIN' WHERE app_users.username = 'admin'");
+        String assignAdminRoleSql = "INSERT INTO user_roles (user_id, role_id) " +
+                "SELECT app_users.id, roles.id " +
+                "FROM app_users " +
+                "JOIN roles ON roles.name = 'ADMIN' " +
+                "LEFT JOIN user_roles ON user_roles.user_id = app_users.id " +
+                "AND user_roles.role_id = roles.id " +
+                "WHERE app_users.username = 'admin' AND user_roles.user_id IS NULL";
+        jdbcTemplate.execute(assignAdminRoleSql);
+
         // Create two tasks and assign to admin and john
-        jdbcTemplate.execute("INSERT INTO tasks (title, description, user_id) SELECT 'Task 1', 'Description of Task 1', app_users.id FROM app_users WHERE app_users.username = 'admin'");
-        jdbcTemplate.execute("INSERT INTO tasks (title, description, user_id) SELECT 'Task 2', 'Description of Task 2', app_users.id FROM app_users WHERE app_users.username = 'john'");
+        String task1Sql = "INSERT INTO tasks (title, description, user_id) " +
+                "SELECT 'Task 1', 'Description of Task 1', app_users.id " +
+                "FROM app_users " +
+                "LEFT JOIN tasks ON tasks.user_id = app_users.id AND tasks.title = 'Task 1' " +
+                "WHERE app_users.username = 'admin' AND tasks.id IS NULL";
+        jdbcTemplate.execute(task1Sql);
+        String task2Sql = "INSERT INTO tasks (title, description, user_id) " +
+                "SELECT 'Task 2', 'Description of Task 2', app_users.id " +
+                "FROM app_users " +
+                "LEFT JOIN tasks ON tasks.user_id = app_users.id AND tasks.title = 'Task 2' " +
+                "WHERE app_users.username = 'john' AND tasks.id IS NULL";
+        jdbcTemplate.execute(task2Sql);
+
     }
 }
 
