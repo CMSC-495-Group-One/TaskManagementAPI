df1adb4c46d34210d1c4c895ff31190f687c3672
ConnorDBurge <connor.d.burge@gmail.com>
2023-08-01 17:38:42 -0400

    service layer started

---
+++ src/test/java/com/group1/taskmanagement/controller/AuthControllerTest.java
@@ -0,0 +1,21 @@
+package com.group1.taskmanagement.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void signup() {
+    }
+
+    @Test
+    void signin() {
+    }
+}
