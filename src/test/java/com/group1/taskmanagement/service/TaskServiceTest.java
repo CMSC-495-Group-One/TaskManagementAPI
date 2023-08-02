b68b902786cc22087699084b545c777e433c44ba
Edward Kwon <kwonedward@gmail.com>
2023-08-01 22:02:41 -0400

    additional tests

--- src/test/java/com/group1/taskmanagement/service/TaskServiceTest.java
+++ src/test/java/com/group1/taskmanagement/service/TaskServiceTest.java
@@ -1,7 +1,6 @@
 package com.group1.taskmanagement.service;
 
 import com.group1.taskmanagement.dto.TaskDto;
-import com.group1.taskmanagement.model.Role;
 import com.group1.taskmanagement.model.Task;
 import com.group1.taskmanagement.model.User;
 import com.group1.taskmanagement.repository.TaskRepository;
@@ -12,7 +11,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -59,22 +57,99 @@ class TaskServiceTest {
 
         assertThat(result.getId()).isEqualTo(task.getId());
         assertThat(result.getUserId()).isEqualTo(user.getUserId());
+        verify(taskRepository).findById(1L);
     }
 
     @Test
     void createTask() {
+        Long userId = 1L;
+        TaskDto taskDto = new TaskDto();
+        taskDto.setUserId(userId);
 
+        User user = new User();
+        user.setUserId(userId);
+
+        Task newTask = new Task();
+        newTask.setUser(user);
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+        when(taskRepository.save(any(Task.class))).thenReturn(newTask);
+
+        taskService.createTask(taskDto);
+
+        verify(userRepository).findById(userId);
+        verify(taskRepository).save(any(Task.class));
     }
 
     @Test
     void updateTask() {
+        Long taskId = 1L;
+        Long userId = 1L;
+        TaskDto taskDto = new TaskDto();
+        taskDto.setUserId(userId);
+
+        User user = new User();
+        user.setUserId(userId);
+
+        Task existingTask = new Task();
+        existingTask.setId(taskId);
+        existingTask.setUser(user);
+
+        Task updatedTaskFromDto = new Task();
+        updatedTaskFromDto.setUser(user);
+
+        when(taskRepository.findById(taskId)).thenReturn(Optional.of(existingTask));
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+        when(taskRepository.save(existingTask)).thenReturn(existingTask);
+
+        TaskDto result = taskService.updateTask(taskId, userId, taskDto);
+
+        verify(taskRepository).findById(taskId);
+        verify(userRepository).findById(userId);
+        verify(taskRepository).save(existingTask);
+        assertThat(result).isNotNull();
     }
+
 
     @Test
     void findAllByUserId() {
+        Long userId = 1L;
+
+        User user = new User();
+        user.setUserId(userId);
+
+        Task task1 = new Task();
+        task1.setId(1L);
+        task1.setUser(user);
+
+        Task task2 = new Task();
+        task2.setId(2L);
+        task2.setUser(user);
+
+        when(taskRepository.findByUser_userId(userId)).thenReturn(List.of(task1, task2));
+
+        List<TaskDto> result = taskService.findAllByUserId(userId);
+
+        verify(taskRepository).findByUser_userId(userId);
+        assertThat(result).hasSize(2);
+        assertThat(result.get(0).getId()).isEqualTo(task1.getId());
+        assertThat(result.get(1).getId()).isEqualTo(task2.getId());
     }
+
 
     @Test
     void deleteById() {
+        Long taskId = 1L;
+        Long userId = 1L;
+
+        Task task = new Task();
+        task.setId(taskId);
+
+        when(taskRepository.findById(taskId)).thenReturn(Optional.of(task));
+
+        taskService.deleteById(taskId, userId);
+
+        verify(taskRepository).findById(taskId);
+        verify(taskRepository).deleteById(taskId);
     }
 }
