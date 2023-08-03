41f3e992d1568a9c6e3a5746752cc683d1197915
ConnorDBurge <connor.d.burge@gmail.com>
2023-08-02 23:16:32 -0400

    wip

--- src/main/java/com/group1/taskmanagement/security/SecurityConfig.java
+++ src/main/java/com/group1/taskmanagement/security/SecurityConfig.java
@@ -77,7 +77,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*")); // specify your origin here
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
         configuration.setAllowedMethods(List.of("GET","POST", "PUT", "DELETE", "HEAD", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
